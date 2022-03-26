f917b04573a1b0d6cf6c4321ccce312f1b14458c
Mohammed Dawood <mahmedtony73@gmail.com>
2022-03-27 00:18:58 +0200

    Adding Basic Validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import React, { useState, useRef } from "react";
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameTnputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (enteredName.trim().length === 0) {
+      return;
+    }
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    // clear the input
+    nameInputRef.current.value = ""; //Not Ideal,don't do this dom manipulation
+    setEnteredName("");
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameTnputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
