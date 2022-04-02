89c989387df25289e380d668e06c0b10b3fa22b8
Mohammed Dawood <mahmedtony73@gmail.com>
2022-04-03 00:26:13 +0200

    Using useReducer() insteadt of useState()

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,29 +1,65 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const intialInputState = {
+  value: "",
+  isTouched: false,
+};
+const intialStateReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT":
+      return {
+        ...state,
+        value: action.value,
+      };
+    case "BLUR":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    case "RESET":
+      return {
+        ...state,
+        value: "",
+        isTouched: false,
+      };
+    default:
+      return intialInputState;
+  }
+  // return intialInputState;
+};
 
 const useInput = (validateInput) => {
+  const [inputState, dispatch] = useReducer(
+    intialStateReducer,
+    intialInputState
+  );
+
   // State
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
 
   // drieven State
-  const valueIsValid = validateInput(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateInput(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   // Handler
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
+    // setEnteredValue(event.target.value);
   };
 
   const valueBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
+    // setIsTouched(true);
   };
 
   const resetInput = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
+    // setEnteredValue("");
+    // setIsTouched(false);
   };
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
